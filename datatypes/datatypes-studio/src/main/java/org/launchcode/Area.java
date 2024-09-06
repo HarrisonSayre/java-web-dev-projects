78abcdb5a2ccf50f230e8771a672ad318028c29d
Harrison Sayre <hsayre42@gmail.com>
2024-09-05 21:24:55 -0400

    Reverted bonus mission 2 to working order after experimentation and cleaned up code.

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -6,20 +6,14 @@ import studios.areaofacircle.Circle;
 public class Area {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a radius:");
         while(!input.hasNextDouble()) {
             System.out.println("Please enter a valid number");
-            input = new Scanner(System.in);
-            //System.exit(0);
+            input.next();
         }
         double radius = input.nextDouble();
-
-        //double area = 3.14 * radius * radius;
         double area = Circle.getArea(radius);
         System.out.println("The area of circle of a radius " + radius + " is: " + area);
-
         input.close();
     }
-
 }
