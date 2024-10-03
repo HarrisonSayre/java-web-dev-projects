1f913521ab3d6625c15ab1653667f114d20f36b9
Harrison Sayre <hsayre42@gmail.com>
2024-10-03 15:55:37 -0400

    Coded along.

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -6,22 +6,61 @@ public class Main {
     public static void main(String[] args) {
         // Test out your Divide() function!
 
+        Divide(1, 0);
+
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
         studentFiles.put("Brad", "");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+        //String nothing = null;
+
+        CheckFileExtension("FILLER");
+
         // Test out your CheckFileExtension() function!
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        if(y == 0) {
+            try {
+                throw new ArithmeticException("You cannot divide by zero.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
+        //int z = x/y;
     }
+
+//    public class FileExtensionException extends NullPointerException{
+//
+//    }
+//
+//    public NullPointerException(String s) {
+//        super(s);
+//    }
 
     public static int CheckFileExtension(String fileName)
     {
         // Write code here!
+        String fileExtension = fileName;
+        fileExtension = null;
+        if(fileExtension == null || fileExtension.isEmpty()) {
+            //System.out.println("EMPTY!");
+            try {
+                throw new NullPointerException();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+            return -1;
+        }
+
+        if(fileExtension.equals(".java")){
+            return 1;
+        }
+        return 0;
     }
+
+
 }
